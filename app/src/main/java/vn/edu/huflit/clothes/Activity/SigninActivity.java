f7fda08b329ba2c72310e120e2cb715bd78ba065
NguyenPhiLong029 <nguyenphilong0299@gmail.com>
2022-05-12 23:31:13 +0700

    signin signup

---
+++ app/src/main/java/vn/edu/huflit/clothes/Activity/SigninActivity.java
@@ -0,0 +1,20 @@
+package vn.edu.huflit.clothes.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import vn.edu.huflit.clothes.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
